daaa5ae065d1a66e03560487bab21c2bc776dc1c
Giorgia <giorgiagranata9@gmail.com>
2024-11-12 10:34:19 +0100

    First profile screen when wardrobe is full

--- OpenCloset/Views/Profile/ProfileView.swift
+++ OpenCloset/Views/Profile/ProfileView.swift
@@ -11,46 +11,67 @@ import Kingfisher
 struct ProfileView: View {
     @ObservedObject var viewModel: ProfileViewModel = ProfileViewModel()
     var profile: Profile
+    @State private var items = Array(1...20) // Initial items
+    @State private var isLoading = false
+    var productList: [String] = []
+    
+    let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
     
     var body: some View {
         VStack {
             HStack {
-                
                 KFImage(URL(string: profile.profileImageURL)) .resizable()
                     .frame(width: 130, height: 130)
                     .clipShape(Circle())
                     .overlay(Circle()
-                    .stroke(Color .white, lineWidth: 2))
+                        .stroke(Color .white, lineWidth: 2))
                     .shadow(radius: 10)
                     .padding(.leading, 20)
                 
                 VStack(alignment: .leading) {
-                    Text("Paola Campanile")
-                        .padding(.bottom,30)
-                        .padding(.leading, 15)
+                    Text("Paolo Campanile")
+                        .padding(.bottom,20)
+                        .padding(.leading, 10)
                         .font(.title2)
+                        .bold()
                     
-
-                    HStack {
-                        Text("150 m")
-                            .padding(5)
-                            .background(Color.pink)
-                            .cornerRadius(5)
-                            .foregroundColor(.white)
-                        
-                        HStack(spacing: 2) {
-                            ForEach(0..<3) { _ in
-                                Image(systemName: "star.fill")
-                                    .foregroundColor(.yellow)
-                            }
-                        }
-                    }
                 }
                 Spacer()
             }
             .padding()
-            
-     
+            if productList.isEmpty {
+                
+            } else {
+                ScrollView {
+                    LazyVGrid(columns: columns, spacing: 10) {
+                        ForEach(items, id: \.self) { item in
+                            KFImage(URL(string: profile.profileImageURL)) //uso KFImage per caricare l'immagine in ogni cella della griglia
+                                .resizable()
+                                .scaledToFill() // per scalare l'immagine
+                                .frame(height: 160) // per l'altezza del rettangolo nella griglia
+                                .cornerRadius(20)
+                            
+                                .cornerRadius(10)
+                        }
+                        
+                        // Show loading indicator at the end of the list
+                        if isLoading {
+                            ProgressView()
+                                .padding()
+                                .onAppear {
+                                    loadMoreData()
+                                }
+                        }
+                    }
+                    .padding()
+                }
+                .onAppear {
+                    loadMoreData() // Load initial data
+                }
+            }
             
             
             
@@ -62,9 +83,24 @@ struct ProfileView: View {
                 Text("Ask Info ♡")
             }
             .buttonStyle(PrimaryButtonStyle())
+            
+        }
+    }
+
+    
+    
+    func loadMoreData() {
+        guard !isLoading else { return }
+        isLoading = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            let moreItems = Array(items.count + 1...items.count + 20)
+            items.append(contentsOf: moreItems)
+            isLoading = false
         }
     }
 }
+
+
 
 #Preview {
     ProfileView(profile: Profile(name: "Paola", surname: "Campanile", email: "paula@campanile.com", about: "Second-hand enthusiast 🌱 ", profileImageURL: "https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-in-shirt-smiles-and-gives-thumbs-up-to-show-approval-png-image_13146336.png"))
