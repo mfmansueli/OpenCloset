82ebe54a8dd86761b1df8d1465322243f705441b
Giorgio <gorgo.durante@gmail.com>
2024-11-12 09:54:16 +0100

    New Empty State and New Search Bar

---
+++ OpenCloset/ViewComponents/EmptyState/EmptyStateView.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyStateView.swift
+//  OpenCloset
+//
+//  Created by Giorgio Durante on 11/11/24.
+//
+
+import SwiftUI
+
+struct EmptyStateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmptyStateView()
+}
